178e76f3c279f77fee57a735a0eafbb1d1ad82e8
grigorkhurshudyan2020 <khurshudyan.grigor123@gmail.com>
2020-11-30 20:18:28 +0100

    initial commit

---
+++ js/main.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+    $('#navbar2').scrollspy();
+
+    $('.owl-carousel').owlCarousel({
+    loop:false,
+    margin:30,
+    nav:true,
+    navText : ["",""],
+    rewindNav : true,
+    responsive:{
+      0:{
+        items:1
+      },
+      600:{
+        items:3
+      },
+      1000:{
+        items:3
+      }
+    }
+    })
+});
